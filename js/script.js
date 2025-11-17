f8a184f566005831bf8f1eb395c4f5f436056a8d
Santana <santanahernandezdehar04@gmail.com>
2025-11-16 19:12:10 -0700

    borrar comentario prueba

--- js/script.js
+++ js/script.js
@@ -5,5 +5,3 @@ menuToggle.addEventListener('click', () => {
   sidebar.classList.toggle('open');
   menuToggle.classList.toggle('active'); //cambia el color del icono
 }); 
-
-//comentario de prueba
